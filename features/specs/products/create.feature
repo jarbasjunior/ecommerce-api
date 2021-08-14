597bb7241e8a9add57b5a8ba0a363961e99db9d5
jarbasjunior <jarbasoujunior@gmail.com>
2021-08-14 14:36:05 -0300

    Adicionado teste para validar proibição de cadastro de produto quanto keyword for inválida

--- features/specs/products/create.feature
+++ features/specs/products/create.feature
@@ -10,4 +10,9 @@ Funcionalidade: Cadastrar produto
   Cenário: Não cadastrar produto sem enviar token na requisição
     Dado que tenho token de administrador
     Quando tento cadastrar um produto com a requisição POST para /products sem o token
-    Então valido resposta da API de proibição de cadastro de produto
+    Então valido resposta da API de proibição de cadastro de produto sem token
+
+  Cenário: Não cadastrar produto com keyword inválida
+    Dado que tenho token de administrador
+    Quando tento cadastrar um produto com a requisição POST para /products com keyword inválida
+    Então valido resposta da API de proibição de cadastro de produto com keyword inválida
--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -18,14 +18,28 @@ Quando('tento cadastrar um produto com a requisição POST para /products sem o 
   @result = $stdin.call(ApiEcommerce).create_product_without_token(@body)
 end
 
+Quando('tento cadastrar um produto com a requisição POST para /products com keyword inválida') do
+  @body = FactoryBot.build(:create_product).to_hash
+  $stdin.call(ApiEcommerce).create_keyword({ word: Faker::Lorem.word }, @token)
+  keywords = $stdin.call(ApiEcommerce).keywords(@token)
+  @body[:keyWords][0] = keywords['data'].last['id'].reverse
+  @result = $stdin.call(ApiEcommerce).create_product_without_token(@body)
+end
+
 Então('valido resposta da API para o cadastro de produto') do
   expect(@result['success']).to    be_truthy
   expect(@result['errors']).to     be_empty
   expect(@result.response.code).to eql '201'
 end
 
-Então('valido resposta da API de proibição de cadastro de produto') do
+Então('valido resposta da API de proibição de cadastro de produto sem token') do
   expect(@result.response.code).to eql '401'
+  expect(@result['success']).to    be_falsey
+  expect(@result['errors']).not_to be_empty
+end
+
+Então('valido resposta da API de proibição de cadastro de produto com keyword inválida') do
+  expect(@result.response.code).to eql '422'
   expect(@result['success']).to    be_falsey
   expect(@result['errors']).not_to be_empty
 end
